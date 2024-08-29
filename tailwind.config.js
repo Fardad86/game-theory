53fd93edbd68ae3eac2ca9f5e53c830709aa3d7d
Fardad86 <136969613+Fardad86@users.noreply.github.com>
2024-08-29 16:50:28 +0330

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        timberwolf: {
+          100: '#ebebea',
+          500: '#d2d2cf',
+        },
+        dark_spring_green: {
+          100: '#9fd9af',
+          500: '#317a46',
+        },
+        oxford_blue: '#09152D',
+        brunswick_green: '#14463F',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
